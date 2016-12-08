ac2c5e87c1ba411756bcf64fe3393f0fe77eb8e6
Wil <wfkoch@gmail.com>
2016-12-08 00:52:52 -0500

    first commit

---
+++ announcements.js
@@ -0,0 +1,8 @@
+var announce = [
+	{
+		"description": '<a href="https://github.com/wil3/hyp3rarmor" target="_blank">Source code</a> for Hyp3rArmor has been released. Protect your website from automated attacks!'
+	},
+	{
+		"description": "Hyp3rArmor was well received at New England Security Day. Looking forward to the next one!"
+	}
+]
---
+++ me-data.js
@@ -0,0 +1,325 @@
+var profile = 
+{
+		"title": "Wil Koch",
+		"portrait": "me-data/pic.jpg",
+		"name": "William Koch",
+		"position": "Cybersecurity Researcher",
+		"bio": "Hello world! My name is Wil Koch (pronounced 'cook') and I am a third year PhD student at Boston University. I am interested in autonomous malware (a.k.a. bots) and developing defenses for their attacks. Before returning to academia I was involved in a few start-ups and worked as a software engineer. I enjoy writting code and working on software projects. When I'm not behind a screen, depending on the season I'll be out snowboarding, surfing or getting lost somewhere in the woods. ",
+		"social" : {
+					"github": "https://github.com/wil3", 
+					"twitter": "https://twitter.com/wil_koch", 
+					"linkedin": "https://www.linkedin.com/in/williamkoch3rd",
+					"keybase": "https://keybase.io/wilkoch",
+					"email": "http://www.google.com/recaptcha/mailhide/d?k=01hMHG3JltR7yEimIcWJvAtg==&c=1hqsuy_ofP7rFxQRbILxxQ==",
+					"resume": "me-data/wilkoch_cv_public.pdf"
+		},
+
+
+		"publications" : [
+			/*
+			{ 
+				"title": "",
+				"authors": "",
+				"location": "",
+				"date": "",
+				"url": "",
+			}
+		    */
+			{ 
+				"title": "Semi-Automated Discovery of Server-Based Information Oversharing Vulnerabilities in Android Applications",
+				"authors": [
+					{
+						"name": "William Koch", 
+					},
+					{
+						"name": "Abdelberi Chaabane", 
+					},
+					{
+						"name": "Manuel Egele", 
+					},
+					{
+						"name": "William Robertson", 
+					},
+					{
+						"name": "Engin Kirda", 
+					},
+				],
+				"publication": "Under review",
+				"talks" : [
+					{
+						"event": "Boston University Security on Practical Security",
+						"date": "Summer 2015"
+					},
+					{
+						"event": "Boston University class on Cybersecurity",
+						"date": "Spring 2015"
+					}
+				]
+			},
+			{ 
+				"title": "PayBreak: Defense Against Cryptographic Ransomware",
+				"authors": [
+					{
+						"name": "Eugene Kolodenker",
+					},
+					{
+						"name": "William Koch", 
+					},
+					{
+						"name": "Gianluca Stringhini", 
+					},
+					{
+						"name": "Manuel Egele", 
+					},
+				],
+				"publication": "Under review",
+				"talks" : [
+					{
+						"event": "Boston University Security on Practical Security",
+						"date": "Summer 2016"
+					}
+				]
+			},
+			{ 
+				"title": "Identifier Binding Attacks and Defenses in Software-Defined Networks",
+				"authors": [
+					{
+						"name": "Samuel Jero"
+					},
+					{
+						"name": "William Koch", 
+					},
+					{
+						"name": "Rickard Skowyra", 
+					},
+					{
+						"name":"Hamed Okhravi",
+					},
+					{
+						"name":"Cristina Nita-Rotaru",
+					},
+					{
+						"name":"David Bigelow",
+					}
+				],
+				"publication": "Under review",
+			},
+			{ 
+				"title": "Hyp3rArmor: Reducing Web Application Exposure to Automated Attacks",
+				"authors" : [
+
+								{
+									"name": "William Koch", 
+								},
+								{
+									"name": "Azer Bestavros", 
+									"url" : "http://azer.bestavros.net/", 
+								},
+				],
+				"publication": "Under review",
+				"date": "November 2016",
+				"url": "me-data/pubs/hyp3rarmor.pdf",
+				"code": "https://github.com/wil3/hyp3rarmor",
+				"talks": [
+					{
+						"event": "New England Security Day",
+						"date": "November 2016"
+					}
+				]
+			},
+			{
+				"title" : "Provide: Hiding From Automated Network Scans With Proofs of Identity",
+				"authors" : [
+
+								{
+									"name": "William Koch", 
+								},
+								{
+									"name": "Azer Bestavros", 
+									"url" : "http://azer.bestavros.net/", 
+								},
+				],
+				"publication": "Hot Topics in Web Systems and Technologies (HotWeb)", 
+				"location": "Washington, DC, USA",
+				"date": "October 2016", 
+				"url" : "me-data/pubs/provide2016.pdf", 
+				"talks": [
+					{
+						"event": "Boston University Network Reading Group",
+						"date": "October 2016"
+					}, 
+					{
+						"event": "New England Networks and Systems Day (NENS)",
+						"date": "October 2016",
+					}
+				]
+			},
+			{
+				"title" : "Markov Modeling of Moving Target Defense Games",
+				"authors" : [
+									{
+										"name": "Hoda Maleki", 
+									},
+									{
+										"name": "Saeed Valizadeh", 
+									},
+
+									{
+										"name": "William Koch", 
+									},
+									{
+										"name": "Azer Bestavros", 
+										"url" : "http://azer.bestavros.net/", 
+									},
+									{
+										"name": "Marten van Dijk", 
+									},
+				],
+				"publication": "Proceedings of the 2016 ACM Workshop on Moving Target Defense", 
+				"location": "Vienna, Austria",
+				"date": "October 2016", 
+				"url" : "me-data/pubs/mtd2016.pdf", 
+			},
+
+			{
+				"title": "Using Bootstrap Aggregated Neural Networks for Peripheral Nerve Injury Treatment.",
+				"authors": [
+					{
+						"name":"Munish Shah",
+					},
+					{	
+						"name":"Wei Chang"
+					},
+					{
+						"name": "Kathleen McGuire"
+					}, 
+					{
+						"name": "William Koch", 
+					},
+					{
+						"name": "Yan Meng"
+					},
+				    {
+						"name":	"Xiaojun Yu"
+					}
+				],
+				"publication": "International Conference on Data Mining (DMIN)",
+				"location": "Las Vegas, Nevada, USA", 
+				"date": "July 2014",
+				"url": "me-data/pubs/dmi2014.pdf",
+			}, 
+
+			{
+				"title": "A Framework for Assisting Learners by Incorporating Knowledge to Aid in Predicting Nerve Guidance Conduit Performance (Masters Thesis)",
+				"authors": [
+					{
+						"name": "William Koch", 
+					},
+				],
+				//"author": "William Koch", 
+				"publication": "Stevens Institute of Technology",
+				"location": "Hoboken, New Jersy, USA",
+				"date": "December 2013",
+				"url" : "me-data/pubs/mastersthesis.pdf"
+			},
+			{
+				"title": "Predicting Nerve Guidance Conduit Performance for Peripheral Nerve Regeneration Using Bootstrap Aggregated Neural Networks",
+				"authors": [
+					{
+						"name": "William Koch", 
+					},
+					{
+						"name": "Yan Meng"
+					},
+					{
+						"name":"Munish Shah",
+					},
+					{	
+						"name":"Wei Chang"
+					},
+				    {
+						"name":	"Xiaojun Yu"
+					}
+				],
+				"publication": "International Joint Conference on Neural Networks (IJCNN)",
+				"location": "Dallas, Texas, USA", 
+				"date": "August 2013",
+				"url" : "me-data/pubs/ijcnn2013.pdf"
+
+			},
+
+		],
+		"industry" : [
+			/*
+			{
+				"position": "",
+				"employer": "",
+				"start": "",
+				"end": "",
+				"description": ""
+			}
+		   */
+
+			{
+				"position": "Security Researcher",
+				"employer": "MIT Lincoln Laboratory",
+				"start": "January 2016",
+				"end": "June 2016",
+				"description": "Developed novel network attacks."
+			},
+			{
+				"position": "CEO",
+				"employer": "Capsules LLC",
+				"start": "October 2013",
+				"end": "August 2014",
+				"description": "Co-founder and CEO of mobile application to augment the world.",
+				"url": "https://capsules.io/"
+			},
+
+			{
+				"position": "Software Engineer",
+				"employer": "Sikorsky Aircraft Contractor",
+				"start": "June 2006",
+				"end": "January 2012",
+				"description": "Lead software engineer on software components in the Sikorsky CH-53K Aircrafs Integrated Support System (ISS). Designed and implemented continuous integration system. Responsible for integration between third-party vendors."
+			},
+			{
+				"position": "Chairman/Co-founder",
+				"employer": "CT Hackerspace",
+				"start": "August 2010",
+				"end": "August 2011",
+				"description": "Co-founder of Connecticuts hackerspace.",
+				"url": "http://www.cthackerspace.com/"
+			}
+
+		],
+
+
+		"projects" : [
+			/*
+			{
+				"title": "",
+				"description": ""
+			   
+			}
+		   */
+			{
+				"title": "Android Leak Hunter",
+				"description": "Automated large scale analysis of Android applications to identify potential information leakages through network connections",
+				"code":"https://github.com/wil3/android-leak-hunter"
+			   
+			},
+			{
+				"title": "DeviceFP",
+				"description": "Fingerprint your Android device to identify information leakages and properties that can be used to uniquely identify your device.",
+				"url": "https://play.google.com/store/apps/details?id=com.github.wil3.devicefp.device&hl=en",
+				"code": "https://github.com/wil3/sandboxed"
+			   
+			},
+			{
+				"title": "CloudSwitch",
+				"description": "Android application to easily toggle the state of your virtual machines by the push of a button. Allows the user to save money by only turning them on when needed.",
+				"url": "https://play.google.com/store/apps/details?id=com.github.wil3.cloudswitch.app&hl=en",
+			}
+		]
+}

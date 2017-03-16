7cf15274ecaf6a767869a66a57fc99dac629a19f
Wil <wfkoch@gmail.com>
2017-03-16 11:29:54 -0400

    Updating Paybreak

--- news.js
+++ news.js
@@ -1,11 +1,8 @@
 var announce = [
 	{
+		"description": "Our ransomware defense as been accepted to AsiaCCS. We've defeated CryptoLocker, SamSam, and Locky, the most dangerous ransomware of 2016!"
+	},
+	{
 		"description": '<a href="https://github.com/wil3/hyp3rarmor" target="_blank">Source code</a> for Hyp3rArmor has been released. Protect your website from automated attacks!'
-	},
-	{
-		"description": "Hyp3rArmor was well received at New England Security Day. Looking forward to the next one!"
-	},
-	{
-		"description": "PayBreak has defeated 2 of the 3 most dangerous ransomware of 2016!"
-	},
+	}
 ]

e19d39ed358e5e61cff2cc5a3303e5cfbbcd5607
Wil <wfkoch@gmail.com>
2017-05-16 11:20:01 -0400

    update news

--- news.js
+++ news.js
@@ -1,10 +1,10 @@
 var announce = [
 	{
-		"description": '<table><tr><td><img src="my-data/images/drone.png" style="width:70%;"/></td><td>I will be starting a Boston Drone Racing and Hacking club this summer, contact me for details.</td></tr></table>' 
+		"description": '<table><tr><td><img src="my-data/images/drone.png" style="width:70%;"/></td><td>Come join us at the <a target="_blank" href="http://www.bostondrone.racing/">Boston Drone Racing and Hacking club</a></td></tr></table>' 
 
 	},
 	{
-		"description": "Our ransomware defense as been accepted to AsiaCCS. We've defeated CryptoLocker, SamSam, and Locky, the most dangerous ransomware of 2016!"
+		"description": "Our ransomware defense, Paybreak, defeats <a target='_blank' href='https://www.eugenekolo.com/blog/paybreak-able-to-defeat-wannacry-wannacryptor-ransomware/'>WannaCry</a>"
 	},
 	{
 		"description": '<a href="https://github.com/wil3/hyp3rarmor" target="_blank">Source code</a> for Hyp3rArmor has been released. Protect your website from automated attacks!'
